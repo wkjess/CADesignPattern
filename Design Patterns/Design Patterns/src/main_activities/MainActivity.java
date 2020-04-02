07d7c64340b3ef8e6e66ffdd9581d0eeffa38d57
kimja <kimja@192.168.1.9>
2020-04-02 23:06:52 +0100

    Finally run the main method successfully.

---
+++ Design Patterns/Design Patterns/src/main_activities/MainActivity.java
@@ -0,0 +1,20 @@
+package main_activities;
+
+import client_programs.TestClientProgram;
+
+/*
+ * Class MainActivity is a main method
+ * which taken the creation of
+ * class TestClientProgram and run it.
+ * @author Kim Jang Wong
+ * Date: 28 March 2020
+ */
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		new TestClientProgram();
+	}
+
+}
